13c978841411ccd35e2306a9df506a6315a9630c
Joshua Martin <hello@joshuamartin.co.uk>
2017-07-06 15:49:22 +0100

    Tidy up indents

--- litespeedcache/services/LiteSpeedCacheService.php
+++ litespeedcache/services/LiteSpeedCacheService.php
@@ -3,25 +3,26 @@ namespace Craft;
 
 class LiteSpeedCacheService extends BaseApplicationComponent
 {
-	public function destroyLiteSpeedCache($dir, $odir = NULL) {
-	   if ($odir == NULL) {
+	public function destroyLiteSpeedCache($dir, $odir = NULL)
+	{
+	  if ($odir == NULL) {
 	   	$odir = $dir;
-	   }
-	   if (is_dir($dir)) {
-	     $objects = scandir($dir);
-	     foreach ($objects as $object) {
-	       if ($object != "." && $object != "..") {
-	         if (is_dir($dir."/".$object))
-	           craft()->liteSpeedCache->destroyLiteSpeedCache($dir."/".$object,$odir);
-	         else
-	           unlink($dir."/".$object);
-	       }
-	     }
-	     if ($odir != $dir) {
-	         rmdir($dir);
-	     }
-	   }
+	  }
+	  if (is_dir($dir)) {
+	    $objects = scandir($dir);
+	    foreach ($objects as $object) {
+	      if ($object != "." && $object != "..") {
+	        if (is_dir($dir."/".$object))
+	          craft()->liteSpeedCache->destroyLiteSpeedCache($dir."/".$object,$odir);
+	        else
+	          unlink($dir."/".$object);
+	      }
+	    }
+	    if ($odir != $dir) {
+	      rmdir($dir);
+	    }
+	  }
 
-	   return true;
-	 }
+	  return true;
+	}
 }
