93a3f5f05efcb9d4301da004470ce6c04247d2ee
Joshua Martin <hello@joshuamartin.co.uk>
2017-12-11 16:49:09 +0000

    Fix documentation URL

--- litespeedcache/LiteSpeedCachePlugin.php
+++ litespeedcache/LiteSpeedCachePlugin.php
@@ -11,7 +11,7 @@ class LiteSpeedCachePlugin extends BasePlugin
 
 	public function getVersion()
 	{
-		return '1.3.0';
+		return '1.3.1';
 	}
 
 	public function getDeveloper()
@@ -36,7 +36,7 @@ class LiteSpeedCachePlugin extends BasePlugin
 
 	public function getDocumentationUrl()
 	{
-	    return 'hhttps://github.com/thoughtfulweb/craft-litespeedcache';
+	    return 'https://github.com/thoughtfulweb/craft-litespeedcache';
 	}
 
 	public function getReleaseFeedUrl()
